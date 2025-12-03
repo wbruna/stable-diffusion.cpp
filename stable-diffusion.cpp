cfcdfaf1d18a7c6d3f7609ff81272ab5e29693c6
Wagner Bruna <wbruna@yahoo.com>
2025-12-03 07:51:48 -0300

    sync with koboldcpp 7316c24bf668

--- stable-diffusion.cpp
+++ stable-diffusion.cpp
@@ -266,9 +266,11 @@ public:
         int tempver = model_loader.get_sd_version();
         bool iswan = (tempver==VERSION_WAN2 || tempver==VERSION_WAN2_2_I2V || tempver==VERSION_WAN2_2_TI2V);
         bool isqwenimg = (tempver==VERSION_QWEN_IMAGE);
+        bool iszimg = (tempver==VERSION_Z_IMAGE);
+        bool isflux2 = (tempver==VERSION_FLUX2);
 
         //kcpp qol fallback: if qwen image, and they loaded the qwen2vl llm as t5 by mistake
-        if(isqwenimg && t5_path_fixed!="")
+        if((isqwenimg||iszimg||isflux2) && t5_path_fixed!="")
         {
             if(clipl_path_fixed=="" && clipg_path_fixed=="")
             {
@@ -300,7 +302,7 @@ public:
                 prefix = "cond_stage_model.transformer.";
                 LOG_INFO("swap clip_vision from '%s'", clipl_path_fixed.c_str());
             }
-            if(isqwenimg)
+            if(isqwenimg||iszimg||isflux2)
             {
                 prefix = "text_encoders.llm.";
                 LOG_INFO("swap llm from '%s'", clipl_path_fixed.c_str());
@@ -413,7 +415,7 @@ public:
             {
                 to_replace = "taesd_xl.embd";
             }
-            else if(sd_version_is_flux(version))
+            else if(sd_version_is_flux(version)||sd_version_is_z_image(version))
             {
                 to_replace = "taesd_f.embd";
             }
