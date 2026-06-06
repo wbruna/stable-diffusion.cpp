0641de99e5639a4ecf35ad57ba77e3470026f5a1
Wagner Bruna <wbruna@yahoo.com>
2026-06-06 16:57:12 -0300

    fix for params -> config refactor

--- src/stable-diffusion.cpp
+++ src/stable-diffusion.cpp
@@ -5958,7 +5958,7 @@ namespace kcpp_sd {
         if (ctx != nullptr && ctx->sd != nullptr) {
             auto maybe_flux = std::dynamic_pointer_cast<Flux::FluxRunner>(ctx->sd->diffusion_model);
             if (maybe_flux != nullptr) {
-                return maybe_flux->flux_params.is_chroma;
+                return maybe_flux->config.is_chroma;
             }
         }
         return false;
